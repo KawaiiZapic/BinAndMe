d609f3e396bd65159e370a885e2304af62cfed49
Zapic <45936772+Zapic233@users.noreply.github.com>
2019-05-18 18:22:57 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,28 @@ func main() {
         if err != nil {
                 fmt.Println(err)
         }
-        var name = usr.Name
+        var name = "|"+usr.Name+"|"
         var nameline = "    =@@@^"
-        //TODO:Count name length and insert into Line
+        var namelen = strings.Count(name,"")-1
+        const spacelen = 27
+        var flen = 0
+        var blen = 0
+        if (namelen % 2 != 0){
+                flen = (spacelen - namelen)/2 - 1
+                blen = flen + 1
+
+        }else{
+                flen = (spacelen - 1 - namelen)/2 - 1
+                blen = flen + 1
+        }
+        for a:=0;a <= flen;a++ {
+                nameline =nameline + " "
+        }
+        nameline = nameline + name
+        for a:=0;a <= blen;a++ {
+                nameline =nameline + " "
+        }
+        nameline = nameline + "=@@@/   "
         fmt.Println("               ,]OOOOOOOOOOO]`              ")
         fmt.Println("             ,@@@@@@@@@@@@@@@@@^            ")
         fmt.Println("            @@@@`            @@@@`          ")
